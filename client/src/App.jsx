7ea751fac91d3029799f14a7c61ec6914f617e5a
Teodora Samardzhieva <tedi1812@abv.bg>
2025-12-10 16:20:50 +0200

    fix app component css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -33,8 +33,18 @@ function App() {
   return (
     <>
       {/* <div className="bg-white"> */}
-      <div>
-        <Background.Top />
+        <div
+          aria-hidden="true"
+          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+        >
+          <div
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+            className="relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-288.75"
+          />
+        </div>
         <Header /> {/* user={user} */}
         <Routes>
           <Route index element={<Home />} />
@@ -58,7 +68,20 @@ function App() {
         </Routes>
         <Footer />
         <Background.Bottom />
-      </div>
+        <div className="relative">
+          <div
+            aria-hidden="true"
+            className="absolute inset-x-0 bottom-0 -z-10 transform-gpu overflow-hidden blur-3xl sm:bottom-0"
+          >
+            <div
+              style={{
+                clipPath:
+                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 10.4% 68.1%, 47.5% 88.3%, 45.2% 34.5%, 7.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+              }}
+              className="relative left-[50%] aspect-[1155/678] w-[144.5rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:w-[188.75rem]"
+            />
+          </div>
+        </div>
     </>
   );
 }
